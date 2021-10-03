c2717ea3501be94f38b8a4abb279876e4004c280
ichi13119 <57549956+ichi13119@users.noreply.github.com>
2021-10-03 11:18:12 +0900

    Minor corrections

--- src/components/ModalWrapper.tsx
+++ src/components/ModalWrapper.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/components/ModalWrapper.module.scss";
+import styles from "../styles/components/modalWrapper.module.scss";
 
 type Props = {
   handleClose?: () => void;
