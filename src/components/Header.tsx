fe2dbfd9395daa888760d187c1a7f895d2f9104a
ichi13119 <57549956+ichi13119@users.noreply.github.com>
2021-10-10 12:20:54 +0900

    Adjust styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,106 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 import styles from "../styles/components/header.module.scss";
 
+const scrollTop = (): number => {
+  return Math.max(
+    window.pageYOffset,
+    document.documentElement.scrollTop,
+    document.body.scrollTop
+  );
+};
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isTop, setIsTop] = useState<boolean>(true);
+
+  const onScroll = (): void => {
+    const position = scrollTop();
+    if (position >= 20) {
+      setIsTop(false);
+    } else {
+      setIsTop(true);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", onScroll);
+    return (): void => document.removeEventListener("scroll", onScroll);
+  });
+
+  const HeaderLinks = () => {
+    return (
+      <ul>
+        <li
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          <Link href="/">
+            <a>HOME</a>
+          </Link>
+        </li>
+        <li
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          <Link href="/about">
+            <a>ABOUT</a>
+          </Link>
+        </li>
+        <li
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          <Link href="/blogs/1">
+            <a>BLOGS</a>
+          </Link>
+        </li>
+        <li
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          <Link href="/contact">
+            <a>CONTACT</a>
+          </Link>
+        </li>
+      </ul>
+    );
+  };
+
   return (
     <header className={styles["c-header"]}>
       <Link href="/">
         <a>SHU ICHIHARA</a>
       </Link>
-      <nav className={styles["c-header__menu"]}>
-          <Link href="/">
-            <a>HOME</a>
-          </Link>
-        <Link href="/about">
-          <a>ABOUT</a>
-        </Link>
-        <Link href="/blogs/1">
-          <a>BLOGS</a>
-        </Link>
-        <Link href="/contact">
-          <a>CONTACT</a>
-        </Link>
-      </nav>
+      <nav className={styles["c-header__menu"]}>{HeaderLinks()}</nav>
+      <button
+        className={styles["c-header__burger"]}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span
+          className={
+            isOpen === true
+              ? `${styles["c-header__burger-line"]} ${styles["open"]}`
+              : isTop === false
+              ? `${styles["c-header__burger-line"]} ${styles["transcolor"]}`
+              : styles["c-header__burger-line"]
+          }
+        ></span>
+      </button>
+      <div
+        className={
+          isOpen === true
+            ? `${styles["c-header__slide"]} ${styles["open"]}`
+            : styles["c-header__slide"]
+        }
+      >
+        {HeaderLinks()}
+      </div>
     </header>
   );
 };
